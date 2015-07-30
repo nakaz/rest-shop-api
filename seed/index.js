c41e16d6ccb6ce89a2c0a44c5992633f09d29ec0
Sean Nakamura <seanenakamura@gmail.com>
2015-07-29 18:52:44 -1000

    working product tables and migrations

---
+++ seed/index.js
@@ -0,0 +1,19 @@
+var models = require('../models');
+
+var faker = require('faker');
+
+models.sequelize
+  .sync({force:true})
+  .then(function(){
+    var products = [];
+    var numberOf = faker.random.number({min:10, max: 30});
+    for(var i = 0; i < numberOf; i++){
+      console.log(faker.commerce.price());
+      products.push({
+        name: faker.commerce.productName(),
+        description: 'It is a ' + faker.commerce.productAdjective().toLowerCase() + ' product made out of ' + faker.commerce.productMaterial().toLowerCase() + '!',
+        price: parseInt(faker.commerce.price(0, 1000, 2))
+      });
+    }
+    return models.Product.bulkCreate(products);
+  });
