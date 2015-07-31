659d362917f0f4b4200faae27cf96b7d4b9ed8c5
Sean Nakamura <seanenakamura@gmail.com>
2015-07-30 19:51:43 -1000

    completed routes for json serving

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var config = require('./config/config.json');
 var restify = require('restify');
+var models = require('./models/');
 
 var server = restify.createServer({
   name: 'Nakz Bodega',
@@ -10,9 +11,50 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/echo/:name', function (req, res, next){
-  res.send(req.params);
-  return next();
+server.get('/products', function (req, res){
+  models.Product
+    .findAll()
+    .then(function (products){
+      return res.json(products);
+    });
+});
+
+server.get('/products/:id', function (req, res){
+  var id = req.params.id;
+  models.Product
+    .findOne({where: {'id': id}})
+    .then(function(product){
+      return res.json(product);
+    });
+});
+
+server.get('/orders', function (req, res){
+  models.Order
+    .findAll()
+    .then(function (orders){
+      return res.json(orders);
+    });
+});
+
+server.get('/orders/:id', function (req, res){
+  var id = req.params.id;
+  models.Order
+    .findOne({where: {'id': id}})
+    .then(function (order){
+      return res.json(order);
+    });
+});
+
+server.post('/orders', function (req, res){
+  models.Order
+    .create({
+      name: req.body.name,
+      quantity: req.body.quantity,
+      product_id: req.body.product_id
+    })
+    .then(function (order){
+      return res.json(order);
+    });
 });
 
 server.listen(8080, function (){
